cd02c525e523bc0a378f4afed298364f70c4f492
zawz <zawz@hotmail.fr>
2020-02-07 13:57:28 +0100

    Init

---
+++ server_deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+. "$(pwd)/.config"
+
+ssh="$SSH_USER@$SSH_ADDR"
+
+# add sources to server
+ssh "$ssh" mkdir -p "$PKG_PATH" || exit $?
+scp .config scripts/* "$ssh":~/ || exit $?
+
+# make zpkg package
+DIR=tmp
+PKG=zpkg
+DEST=/usr/local/bin
+BASHDEST=/etc/bash_completion.d
+mkdir -p "$DIR/$PKG$DEST" || exit $?
+mkdir -p "$DIR/$PKG$BASHDEST" || exit $?
+cp src/zpkg.bash "$ZPKG_PKG_PATH/$PKG$BASHDEST" || exit $?
+cp src/zpkg "$ZPKG_PKG_PATH/$PKG$DEST" || exit $?
+cd pkg/zpkg || exit $?
+tar -cvJf zpkg.tar.xz * || exit $?
+# send package
+scp zpkg.tar.xz "$ssh":~/"$PKG_PATH" || exit $?
+rm -rd "$DIR"
+# update database
+ssh "$ssh" sh database_update.sh || exit $?
+# generate install script
+ssh "$ssh" sh gen_install.sh || exit $?
---
+++ src/zpkg.bash
@@ -0,0 +1,16 @@
+#/usr/bin/env bash
+
+_zpkg_completion()
+{
+  _cw1="deploy update update-database install remove fetch show list list-all list-outdated list-removed"
+  _cw1_pkgw="install remove fetch show"
+  _cw1_file="deploy"
+  if [ "$COMP_CWORD" = "1" ] ; then
+    _compwords=$_cw1
+  elif [ "$COMP_CWORD" -gt "1" ] && [ -n "$(echo "$_cw1_pkgw" | grep -w "${COMP_WORDS[1]}")" ] ; then
+    _compwords=$(zpkg list-all)
+  fi
+  COMPREPLY=($(compgen -W "$_compwords" "${COMP_WORDS[$COMP_CWORD]}"))
+}
+
+complete -F _zpkg_completion -o dirnames zpkg
