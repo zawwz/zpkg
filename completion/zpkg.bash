1556501efdd3e31f6a99148b663913927f62537f
Mateo FERON <mateo.feron@elipce.com>
2020-04-14 11:34:11 +0200

    Discard completion errors

--- completion/zpkg.bash
+++ completion/zpkg.bash
@@ -7,10 +7,10 @@ _zpkg_completion()
   _cw1_file="deploy"
   if [ "$COMP_CWORD" = "1" ] ; then
     _compwords=$_cw1
-  elif [ "$COMP_CWORD" -gt "1" ] && [ -n "$(echo "$_cw1_pkgw" | grep -w "${COMP_WORDS[1]}")" ] ; then
-    _compwords=$(zpkg list-all)
+  elif [ "$COMP_CWORD" -gt "1" ] && [ -n "$(echo "$_cw1_pkgw" | grep -w "${COMP_WORDS[1]}" 2>/dev/null)" ] ; then
+    _compwords=$(zpkg list-all 2>/dev/null)
   fi
-  COMPREPLY=($(compgen -W "$_compwords" "${COMP_WORDS[$COMP_CWORD]}"))
+  COMPREPLY=($(compgen -W "$_compwords" "${COMP_WORDS[$COMP_CWORD]}" 2>/dev/null))
 }
 
 complete -F _zpkg_completion -o dirnames zpkg
